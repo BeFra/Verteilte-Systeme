258873622e5c97928a729751e55e7a72d23f471a
BeFra <BeFra@users.noreply.github.com>
2015-03-04 15:04:16 +0100

    Update LocalViewForwarder.java

--- 6-Aufgabe/src/blatt6/LocalViewForwarder.java
+++ 6-Aufgabe/src/blatt6/LocalViewForwarder.java
@@ -8,12 +8,9 @@ import forum.framework.Position;
 
 public class LocalViewForwarder implements IForumView
 {
-	
 	@Override
 	public void notifyView(Map<String, Position> folks) throws IOException
 	{
 		LocalViewReceiver.getInstance().notifyView(folks);
-		
 	}
-
 }
